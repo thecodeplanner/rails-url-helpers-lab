78749e4e31156e1fceb6b00aa9b5be9534cb3ea5
thecodeplanner <thecodeplanner@gmail.com>
2020-11-16 18:17:23 -0500

    commit tree a69fbf53ba1e7d7d53ef29e5a6307119a94194df

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,14 +3,18 @@ class StudentsController < ApplicationController
   
   def index
     @students = Student.all
+    render :index 
   end
 
   def show
+    @student = Student.find(params[id])
   end
 
-  private
 
-    def set_student
-      @student = Student.find(params[:id])
-    end
+  def activate
+    @student = Student.find(params[:id])
+    @student.active = !@student.active 
+    @student.save
+    redirect_to student_path(@student)
+  end
 end
